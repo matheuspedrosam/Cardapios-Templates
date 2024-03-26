dd3ae40ece237743f27b0bd53915f35518f0909e
matheuspedrosam <matheuspedrosa2002@gmail.com>
2024-03-26 15:35:46 -0300

    first commit

---
+++ public/assets/js/dadosMockados.js
@@ -0,0 +1,50 @@
+export const cardapio = [
+    {
+      categoria: "Entrada",
+      nome: "Carpaccio de Beterraba",
+      descricao: "desc...",
+      preco: 15.00
+    },
+    {
+      categoria: "Entrada",
+      nome: "Camarões ao Alho",
+      descricao: "desc...",
+      preco: 25.00
+    },
+    {
+      categoria: "Entrada",
+      nome: "Bruschettas de Tomate e Manjericão",
+      descricao: "desc...",
+      preco: 18.00
+    },
+    {
+      categoria: "Prato Principal",
+      nome: "Risoto de Cogumelos Selvagens",
+      descricao: "desc...",
+      preco: 30.00
+    },
+    {
+      categoria: "Prato Principal",
+      nome: "Filet Mignon ao Molho de Vinho Tinto",
+      descricao: "desc...",
+      preco: 45.00
+    },
+    {
+      categoria: "Sobremesa",
+      nome: "Tiramisù Clássico",
+      descricao: "desc...",
+      preco: 20.00
+    },
+    {
+      categoria: "Bebida",
+      nome: "Caipirinha de Limão",
+      descricao: "desc...",
+      preco: 12.00
+    },
+    {
+      categoria: "Bebida",
+      nome: "Vinho Tinto Malbec",
+      descricao: "desc...",
+      preco: 60.00
+    }
+  ];
